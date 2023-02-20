646ef56f966ba52bd2003ca8c3ab968ff5574a60
Ivan <pl_st@list.ru>
2023-02-20 14:38:57 +0300

    feat: added customDatePicker

---
+++ src/components/ui/CustomDatePicker.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="date-picker">
+    <label>{{ title }}</label>
+    <input
+        v-model="date"
+        type="number"
+        min="1900"
+        max="2099"
+        step="1"
+        class="form-control w-100"
+        @change="emits('date-picked', date)"
+    />
+  </div>
+</template>
+
+<script setup>
+  import {ref} from "vue";
+
+  defineProps(['title'])
+  const emits = defineEmits(['date-picked'])
+
+  let date = ref(null)
+</script>
